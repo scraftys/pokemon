2474e1fa56f7949d6494de4f3914b0f35e888e3c
scraftys <mochizou@aol.com>
2017-01-03 07:00:44 -0500

    Update config.js

--- static/config.js
+++ static/config.js
@@ -27,4 +27,4 @@ var contactUrl = "http://reddit.com/u/lunarinterlude";
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/9807f540-add0-11e6-8471-0aa7cfe732d3.png";
